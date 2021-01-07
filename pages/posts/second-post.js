ac38ebbfd71929867e3d8db600fa22c7bfebfb02
Jorge-Bill <jorge.mendesx@gmail.com>
2021-01-07 12:04:13 -0300

    page build with tailwind css

--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -87,8 +87,7 @@ const SecondPost = () => {
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block xl:inline">Data to enrich your</span>
-                  <span className="block text-indigo-600 xl:inline">online business</span>
+                  <span className="block text-indigo-600 xl:inline">Afrodite cartomante</span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
@@ -96,7 +95,7 @@ const SecondPost = () => {
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <a href="/" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-                      Get started
+                      Contato
                     </a>
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
@@ -118,9 +117,9 @@ const SecondPost = () => {
       <div className="py-12 bg-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="lg:text-center">
-            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Baralhos</h2>
             <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              A better way to send money
+              Conheça os tipos de tarot que jogo
             </p>
             <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
               Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
@@ -212,13 +211,13 @@ const SecondPost = () => {
       <div className="bg-gray-50">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block">Ready to dive in?</span>
-            <span className="block text-indigo-600">Start your free trial today.</span>
+            <span className="block">Vamos marcar uma consulta?</span>
+            <span className="block text-indigo-600">Acessar meu whattsapp.</span>
           </h2>
           <div className="mt-8 lex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md shadow">
               <a href="/" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-                Get started
+                Conversar
               </a>
             </div>
             <div className="ml-3 inline-flex rounded-md shadow">
